3984142b829d04a7c97d9f9b069130e4bae1a3b7
Julia Saari <mariajulia.saari@gmail.com>
2020-10-07 11:24:46 +0300

    unicafe userstate button ok

--- part1/unicafe/unicafe/src/index.js
+++ part1/unicafe/unicafe/src/index.js
@@ -2,15 +2,32 @@
  import ReactDom from 'react-dom'
 
  const Header = ({text}) => <h1>{text}</h1>
+
+ const Button = ({handleClick, text}) => {
+
+  return(
+    <button onClick={handleClick}>{text}</button>
+  )
+
+ }
+
+
  const App = () => {
    // save clicks of each button to its own state 
    const [good, setGood] = useState(0)
    const [neutral, setNeutral] = useState(0)
    const [bad, setBad] = useState(0)
 
+   const addGoodFeedback = () => {
+     setGood(good + 1)
+   }
+
    return (
      <div>
        <Header text='give feedback'/>
+       {good}
+       <Button handleClick={addGoodFeedback} text='good'/>
+       <Header text='statistics'/>
 
      </div>
    )
