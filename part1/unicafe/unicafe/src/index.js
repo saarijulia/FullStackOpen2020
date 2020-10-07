be3e2f0d6220fb8396b121496533da77ca33b066
Julia Saari <mariajulia.saari@gmail.com>
2020-10-07 11:45:50 +0300

    1.6 ok

--- part1/unicafe/unicafe/src/index.js
+++ part1/unicafe/unicafe/src/index.js
@@ -11,6 +11,12 @@
 
  }
 
+ const Statistic = (feedback) => {
+   return (
+     <p>{feedback.name} {feedback.value}</p>
+   )
+ }
+
 
  const App = () => {
    // save clicks of each button to its own state 
@@ -18,16 +24,33 @@
    const [neutral, setNeutral] = useState(0)
    const [bad, setBad] = useState(0)
 
+   const addFeedback = (feedback, setter) => {
+     setter(feedback +1)
+   }
+/*
    const addGoodFeedback = () => {
-     setGood(good + 1)
+     setGood(good +1)
    }
 
+   const addBadFeedback = () => {
+     setBad(bad +1)
+   }
+
+   const addNeutralFeedback = () => {
+     setNeutral(neutral + 1)
+   }
+*/
    return (
      <div>
        <Header text='give feedback'/>
-       {good}
-       <Button handleClick={addGoodFeedback} text='good'/>
+       <Button handleClick={() =>addFeedback(good, setGood)} text='good'/>
+       <Button handleClick={() => addFeedback(neutral, setNeutral)} text='neutral'/>
+       <Button handleClick={() => addFeedback(bad, setBad)} text='bad'/>
+       
        <Header text='statistics'/>
+       <Statistic name = 'good' value = {good} />
+       <Statistic name = 'neutral' value = {neutral} />
+       <Statistic name = 'bad' value = {bad} />
 
      </div>
    )
