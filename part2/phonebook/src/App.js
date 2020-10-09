8de80f6d22baa4f2e177d207565dd62553dc6dd4
Julia Saari <mariajulia.saari@gmail.com>
2020-10-09 17:01:33 +0300

    part2.9_ok

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,43 @@
 import React, { useState } from 'react'
 
+const PersonsList = (props) => {
+
+    if (props.textSearch != '') {
+        return (
+            <div>
+                {props.newFilter.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                {props.persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+            </div>
+        )
+    }
+}
+
 const App = () => {
 
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
 
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [newFilter, setFilter] = useState(persons)
+    const [textSearch, setTextSearch] = useState('')
+
+
+    const changeFilter = (event) => {
+        setFilter(persons.filter(function (person) {
+            return person.name.toLowerCase().startsWith(event.target.value.toLowerCase())
+        }))
+        setTextSearch(event.target.value)
+    }
 
     // event handler to get the input and set that as newName
     const changeName = (event) => {
@@ -41,17 +71,24 @@ const App = () => {
         setNewNumber('')
     }
 
+
+
+
     return (
         <div>
             <h2>
                 Phonebook
             </h2>
+            <div>
+                filter shown with <input onChange={changeFilter} />
+            </div>
+
+            <h2>add a new</h2>
             <form onSubmit={addName}>
                 <div>
                     name: <input value={newName} onChange={changeName} />
                 </div>
-                <div>number: <input value={newNumber} onChange={changeNumber}/></div>
-                <div>debug: {newNumber}</div>
+                <div>number: <input value={newNumber} onChange={changeNumber} /></div>
                 <div>
                     <button type="submit">
                         add
@@ -59,11 +96,10 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            <div>
-    {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
-            </div>
+            <PersonsList persons={persons} textSearch={textSearch} newFilter={newFilter} />
         </div>
     )
 }
 
 export default App
+
