2a485b13fa63dc92f093ec3506233ad3008ef7f1
Julia Saari <mariajulia.saari@gmail.com>
2020-10-16 13:21:20 +0300

    2.13_refactor_ok

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,10 +3,9 @@ import axios from 'axios'
 import CountriesDisplay from './Components/CountriesDisplay';
 import SearchText from './Components/SearchText'
 import CountryView from './Components/CountryView'
-
+import Header from './Components/Header'
 
 const App = () => {
-
 
   const [countries, setCountries] = useState([])
   const [filterCountries, setFilterCountries] = useState(countries)
@@ -26,63 +25,47 @@ const App = () => {
   if (filterCountries.length>10) {
     return (
       <div>
-      <h1>
-        countries
-      </h1>
+      <Header text="countries"/>
       <SearchText setFilterCountries={setFilterCountries} 
         countries={countries} 
         setShowCountryView={setShowCountryView}
         showCountryView={showCountryView}/>
-      <div></div>
       <p>Too many results, filter search</p>
       </div>
       )
-
   }
   else if (filterCountries.length===1) {
     return (
       <div>
-      <h1>
-        countries
-      </h1>
+     <Header text="countries"/>
       <SearchText setFilterCountries={setFilterCountries} 
         countries={countries} 
         setShowCountryView={setShowCountryView}
         showCountryView={showCountryView}/>
-      <div></div>
       <CountryView country={filterCountries[0]}/>
       </div>
     )
   } else if (showCountryView){
     return (
       <div>
-      <h1>
-        countries
-      </h1>
+     <Header text="countries"/>
       <SearchText setFilterCountries={setFilterCountries} 
         countries={countries} 
         setShowCountryView={setShowCountryView}
         showCountryView={showCountryView}/>
-      <div></div>
       <CountryView country={selectedCountry}/>
       </div>
     )
   }
-
   return (
     <div>
-      <h1>
-        countries
-      </h1>
+      <Header text='countries'/>
       <SearchText setFilterCountries={setFilterCountries} 
         countries={countries} 
         setShowCountryView={setShowCountryView}
         showCountryView={showCountryView}/>
-      <div>
       <CountriesDisplay countries={filterCountries} showCountryView={showCountryView}
         setShowCountryView = {setShowCountryView} setSelectedCountry={setSelectedCountry}/>
-      </div>
-      
     </div>
 
   )
