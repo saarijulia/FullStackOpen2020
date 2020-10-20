a042c03948571d0614831b4ef270eff19d379b52
Julia Saari <mariajulia.saari@gmail.com>
2020-10-20 15:50:12 +0300

    ex 2.17 ok

--- part2/phonebook/src/services/phonenumbers.js
+++ part2/phonebook/src/services/phonenumbers.js
@@ -10,15 +10,21 @@ const addName = (newNumber) => {
     return axios.post(baseUrl, newNumber)
 }
 
-const deleteNumber = (id) => {
-    console.log(`${baseUrl}/${id.id}`);
-    return (
-    axios.delete(`${baseUrl}/${id.id}`)
-        .catch(error => {
-            console.log('person does not exist in system')
-            alert('This person does not exist in phonebook')
-        })
-    )
+const deleteNumber = (person) => {
+    console.log(`${baseUrl}/${person.id}`);
+    const result = window.confirm(`Do you want to delete ${person.name}?`)
+    if (result) {
+        return (
+            axios.delete(`${baseUrl}/${person.id}`)
+                .catch(error => {
+                    console.log('person does not exist in system')
+                    alert('This person does not exist in phonebook')
+                })
+            )
+    }
+
+
+   
 }
 
 export default {getAll, addName, deleteNumber}
